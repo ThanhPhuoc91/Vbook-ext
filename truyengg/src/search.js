26246f8f5bf7ce5988eb0a2467f6fde8bb1b3b07
ThanhPhuoc91 <117423408+ThanhPhuoc91@users.noreply.github.com>
2024-11-19 17:20:29 +0700

    Add files via upload

---
+++ truyengg/src/search.js
@@ -0,0 +1,31 @@
+load('config.js');
+function execute(key, page) {
+    if (!page) page = '1';
+    let response = fetch(BASE_URL +"/tim-kiem/",{
+        queries: {
+            q: key,
+            trang: page
+        }
+    });
+    if (response.ok) {
+	let doc = response.html();
+        var novelList = [];
+        var next = doc.select(".pagination").select("a.active + a").last().text();
+        doc.select(".list_item_home .item_home").forEach(e => {
+            var cover = e.select(".image-cover img").attr("data-src");
+            if (cover.startsWith("//")) {
+                cover = "http:" + cover;
+            }
+            novelList.push({
+                name: e.select("a.book_name").text(),
+                link: e.select("a.book_name").first().attr("href"),
+                description: e.select('a').last().text(),
+                cover: cover,
+                host: BASE_URL
+            });
+        })
+        return Response.success(novelList, next)
+    }
+
+    return null;
+}

28b6b24f80372851fa5386ff3eb3c6f3f78f09cd
ThanhPhuoc91 <117423408+ThanhPhuoc91@users.noreply.github.com>
2025-01-24 11:16:04 +0700

    Add files via upload

--- truyenqq/src/gen.js
+++ truyenqq/src/gen.js
@@ -2,8 +2,8 @@ load('bypass.js');
 load('config.js');
 function execute(url, page) {
     if (!page) page = '1';
-    url = url.replace(".html", "") + "/trang-" + page + ".html";
-    var doc = bypass(BASE_URL + url, fetch(BASE_URL + url).html());
+    url = BASE_URL + url.replace(".html", "/trang-" + page + ".html");
+    var doc = bypass(url, fetch(url).html());
 
     if (doc) {
         var novelList = [];
@@ -11,14 +11,14 @@ function execute(url, page) {
         doc.select("#main_homepage .list_grid li").forEach(e => {
             var cover = e.select(".book_avatar img").attr("src");
             if (cover.startsWith("//")) {
-                cover = "http:" + cover;
+                cover = "https:" + cover;
             }
             novelList.push({
                 name: e.select(".book_name").text(),
                 link: e.select(".book_name a").first().attr("href"),
-                description: e.select(".last_chapter").text(),
+                description: `${e.select(".last_chapter").text()} - ${e.select("span.time-ago").text()}`,
                 cover: cover,
-                host: BASE_URL
+                host: BASE_URL,
             });
         })
 
